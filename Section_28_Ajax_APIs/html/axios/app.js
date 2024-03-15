034b3d128b3c03c4b549d7a0bd5d9f9923fdb9ea
mavillares <ma.villares@gmail.com>
2024-03-15 14:04:37 +0000

    examples of ajax apis

---
+++ Section_28_Ajax_APIs/html/axios/app.js
@@ -0,0 +1,41 @@
+axios
+  .get("https://swapi.dev/api/people/1")
+  .then((res) => {
+    console.log("REsPONSE:", res);
+  })
+  .catch((e) => {
+    console.log("ERROR!!", e);
+  });
+
+const getStarWarsPerson = async (id) => {
+  try {
+    const res = await axios.get(`https://swapi.dev/api/people/${id}`);
+    console.log(res.data);
+  } catch (e) {
+    console.log("ERROR:", e);
+  }
+};
+
+getStarWarsPerson(2);
+getStarWarsPerson(5);
+getStarWarsPerson(10);
+
+const jokes = document.querySelector("#jokes");
+const button = document.querySelector("button");
+
+const addNewJoke = async () => {
+  const jokeTExt = await getDadJoke();
+  const newLI = document.createElement("LI");
+  newLI.append(jokeTExt);
+  jokes.append(newLI);
+};
+const getDadJoke = async () => {
+  try {
+    const config = { headers: { Accept: "application/json" } };
+    const res = await axios.get("https://icanhazdadjoke.com/", config);
+    return res.data.joke;
+  } catch (e) {
+    return "NO JOKES AVAILABLE! SORRY :(";
+  }
+};
+button.addEventListener("click", addNewJoke);
