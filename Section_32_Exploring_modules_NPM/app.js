e923f790d6dc7d72a334bfe9f949b88130591540
mavillares <ma.villares@gmail.com>
2024-03-19 07:28:05 +0000

    installing npm packages

---
+++ Section_32_Exploring_modules_NPM/app.js
@@ -0,0 +1,8 @@
+const math = require("./math");
+
+const cats = require("./shelter");
+/*console.log(math.PI);
+
+console.log(math.square(9));*/
+
+console.log("REQUIRED AN ENTIRE DIRECTORY!", cats);
