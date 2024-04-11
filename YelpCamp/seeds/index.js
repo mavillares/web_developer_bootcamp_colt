561380eeebe921b7fe48bab1f1256c1443f9d9b6
mavillares <ma.villares@gmail.com>
2024-04-11 12:10:22 +0000

    adding basic styles

--- YelpCamp/seeds/index.js
+++ YelpCamp/seeds/index.js
@@ -23,9 +23,14 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       location: `${cities[random1000].city},${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: "https://source.unsplash.com/random?camp",
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio ratione dolorum voluptate quidem, perspiciatis, cumque eum enim fugiat, beatae aliquam non nostrum placeat ex fugit deleniti ipsa neque esse quo!",
+      price,
     });
     await camp.save();
   }
