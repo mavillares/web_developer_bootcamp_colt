814712e83cd098ebd881de08c2c826729959bb7e
mavillares <ma.villares@gmail.com>
2024-05-10 16:58:04 +0000

    fancy cluster map

--- YelpCamp/seeds/index.js
+++ YelpCamp/seeds/index.js
@@ -21,7 +21,7 @@ const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
   await Campground.deleteMany({});
-  for (let i = 0; i < 50; i++) {
+  for (let i = 0; i < 300; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
@@ -32,7 +32,13 @@ const seedDB = async () => {
       description:
         "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio ratione dolorum voluptate quidem, perspiciatis, cumque eum enim fugiat, beatae aliquam non nostrum placeat ex fugit deleniti ipsa neque esse quo!",
       price,
-      geometry: { type: "Point", coordinates: [-113.1331, 47.0202] },
+      geometry: {
+        type: "Point",
+        coordinates: [
+          cities[random1000].longitude,
+          cities[random1000].latitude,
+        ],
+      },
       images: [
         {
           url: "https://res.cloudinary.com/dqrucw3oz/image/upload/v1714726009/YelpCamp/q6baps4xnvehcbuey7iy.avif",
